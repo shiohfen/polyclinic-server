8a82fa3492b9914b6daba4d22af19fee5b488e83
trollutrollu <trolledit6@gmail.com>
2022-10-19 03:22:59 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
 const admin = require('firebase-admin');
@@ -20,37 +21,38 @@ var serviceAccount = {
   "client_x509_cert_url": process.env.client_x509_cert_url,
 }
 
-
+// const sa = require('./serviceAccountKey.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 app.use(cors());
+app.use(bodyParser.json())
 
 app.post('/registerUser', (req, res) => {
+
   admin.auth().createUser({
-    email: req.email,
+    email: req.body.email,
     emailVerified: false,
-    phoneNumber: req.contact,
-    password: req.password,
-    displayName: req.firstName + ' ' + req.lastName,
-    photoURL: req.photoURL,
+    phoneNumber: req.body.contact,
+    password: req.body.password,
+    displayName: req.body.firstName + ' ' + req.lastName,
+    photoURL: req.body.photoURL,
     disabled: false,
   })
     .then(async (userRecord) => {
       // console.log('Successfully created new user:', userRecord.uid);
-
-      admin.auth().generateEmailVerificationLink(userRecord.email)
-        .then(() => {
-          res.send("Email Verification Sent!")
-
-        })
-        .catch((err) => {
-          res.send("Error sending Email Verification!", err);
-
-        }
-        )
+      res.send('Successfully Created User!')
+      // admin.auth().generateEmailVerificationLink(userRecord.email,)
+      //   .then((link) => {
+      //     // Construct email verification template, embed the link and send
+      //     res.send('Email Verification Sent!'+ link)
+      //   })
+      //   .catch((error) => {
+      //     // Some error occurred.
+      //     console.log('Error sending Email Verification!:', error);
+      //   });
     })
     .catch((error) => {
       console.log('Error creating new user:', error);
@@ -58,7 +60,7 @@ app.post('/registerUser', (req, res) => {
 });
 
 app.post('/disableUser', (req, res) => {
-  admin.auth().updateUser(req.uid, {
+  admin.auth().updateUser(req.body.uid, {
     disabled: true,
   })
     .then((userRecord) => {
@@ -71,12 +73,12 @@ app.post('/disableUser', (req, res) => {
 });
 
 app.post('/enableUser', (req, res) => {
-  admin.auth().updateUser(req.uid, {
+  admin.auth().updateUser(req.body.uid, {
     disabled: false,
   })
     .then((userRecord) => {
       // See the UserRecord reference doc for the contents of userRecord.
-      console.log('Successfully updated user', req.uid);
+      console.log('Successfully updated user', req.body.uid);
     })
     .catch((error) => {
       conle.log('Error creating new user:', error);
@@ -85,7 +87,7 @@ app.post('/enableUser', (req, res) => {
 
 app.post('/deleteUser', (req, res) => {
   admin.auth()
-    .deleteUser(req.uid)
+    .deleteUser(req.body.uid)
     .then(() => {
       console.log('Successfully deleted user');
     })
@@ -101,3 +103,7 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 5000, () => {
   console.log('Listening on port: ' + process.env.PORT || 5000);
 })
+
+// app.listen(5000, () => {
+//   console.log('Listening on port: ' + 5000);
+// })
