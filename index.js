04425918a0c52c60572761375aa973570a4e282d
trollutrollu <trolledit6@gmail.com>
2022-10-20 02:41:42 +0800

    update

--- index.js
+++ index.js
@@ -102,6 +102,20 @@ app.post('/deleteUser', (req, res) => {
     });
 });
 
+app.post('/updateEmail', (req, res) => {
+  admin.auth()
+    .updateUser(req.body.uid, {
+      email: req.body.email,
+    })
+    .then((userRecord) => {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log('Successfully updated user', userRecord.toJSON());
+    })
+    .catch((error) => {
+      console.log('Error updating user:', error);
+    });
+});
+
 app.get('/', (req, res) => {
   res.send('please work');
 });
